0ada0b74d3c7569b2cded3ee6c288d6f66d394cd
Filip Byrén <34345786+FilipByren@users.noreply.github.com>
2018-11-17 17:19:48 +0100

    Add files via upload

---
+++ Generate_report.py
@@ -0,0 +1,37 @@
+from google.cloud import bigquery
+import datetime
+import random
+import os
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "credentials/credential.json"
+
+bigquery_client = bigquery.Client()
+
+
+def aggregation_on_table():
+    # Table is from: https://www.w3schools.com/sql/
+    query = """
+    SELECT SUM(CustomerID) as random_value , MAX(CustomerID) as max
+    FROM
+    (SELECT CustomerID
+    FROM dashboard_db.customer 
+    ORDER BY RAND()
+    LIMIT 10)
+    """
+    query_job = bigquery_client.query(query)
+    df = query_job.to_dataframe()
+    random_day = random.randint(1, 28)
+    random_month = random.randint(1, 12)
+    df["date"] = datetime.datetime.now().replace(day=random_day,month=random_month)
+    return df
+
+
+def load_on_dashboard_table(data_frame):
+    destination_table = "dashboard_db.dashboard_table"
+    project_id = "bi-dashboard-automated-example"
+    data_frame.to_gbq(destination_table, project_id, if_exists='append')
+
+
+if __name__ == '__main__':
+    df = aggregation_on_table()
+    load_on_dashboard_table(df)
